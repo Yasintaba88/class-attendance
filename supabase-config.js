f048b62087bae5ec2a3b1e224210810060611f19
Yasintaba88 <tabatabayisayedyasin035@gmail.com>
2026-09-25 19:25:44 +0330

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://viihjrasitrvcwxsbbql.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "اینجا کلید Publishable خودت را بگذار";
+const SUPABASE_PUBLISHABLE_KEY = "19121912";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
